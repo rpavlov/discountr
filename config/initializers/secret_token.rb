71cde6aa0f76fe65aec59e142bfa0e8e04f5806b
rpavlov <arepavlov@gmail.com>
2013-02-15 19:42:03 -0500

    Initial commit. Gonna sell lots of widgets.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Discountr::Application.config.secret_token = '0a80b59cbfc67b173ff8fcf884d0faba618eaa751650195da1ad5c0267009d01de984e5e5ce0906c4b48abe672b0cf9e86aff46ee9bb08b3968a7ee48d800218'
